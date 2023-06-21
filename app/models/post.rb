ca80dc3ce4dc5029c80905a24c83cbe00521cfdf
gorety <wagutugorety@gmail.com>
2023-06-21 12:49:15 +0300

    assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,23 @@
-class Post < ApplicationRecord
-    validates :title, presence: true
-    validates :content, length: { minimum: 250 }
-    validates :summary, length: { maximum: 250 }
-    validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
-    validate :sufficiently_clickbait_title
-    
 
-    private
-  
-    def sufficiently_clickbait_title
-      return if title.blank?
-  
-      clickbait_keywords = ['Won\'t Believe', 'Secret', /^Top \d+$/, 'Guess']
-      unless clickbait_keywords.any? { |keyword| keyword.match?(title) }
-        errors.add(:title, 'must be clickbait-y')
-      end
+class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  validate :clickbait?
+
+  private
+
+  def clickbait?
+    clickbait_keywords = [
+      /Won't Believe/i,
+      /Secret/i,
+      /^Top \d/i,
+      /Guess/i
+    ]
+
+    if clickbait_keywords.none? { |pattern| pattern.match?(title) }
+      errors.add(:title, 'must be clickbait')
     end
+  end
 end
